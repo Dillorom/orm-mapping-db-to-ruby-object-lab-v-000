29aa400859fc2551c556e13c54ea95bd6e6806db
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-22 23:34:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT * name FROM students
+    SELECT * FROM students
     WHERE grade <= 11
     SQL
     DB[:conn].execute(sql)
