ced1f82b4c2b316bb916ef4e8763ef2aee507a3e
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-22 23:45:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,10 +57,11 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql = <<-SQL
-    SELECT * FROM students
-    WHERE grade <= 11
-    SQL
-    DB[:conn].execute(sql)
+    # sql = <<-SQL
+    # SELECT * FROM students
+    # WHERE grade <= 11
+    # SQL
+    # DB[:conn].execute(sql)
+    self.all.detect {|student| student.grade <= "11"}
   end
 end
